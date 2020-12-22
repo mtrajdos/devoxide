610d68b38d6bc909b095997f7be9df60ca9f86ac
michal.trajdos <michal.trajdos@fdmgroup.com>
2020-12-22 13:29:06 +0000

    Created topbar class and styling.css file

--- solo-projects/react/nextjs/dm-design/pages/index.js
+++ solo-projects/react/nextjs/dm-design/pages/index.js
@@ -2,18 +2,20 @@ import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../styling.css';
  
-export default class Home extends React.Component {
+export default class Home extends Component {
 
   render() {
 
-    return (<Container>
+    return (<div class="container">
       <Row>
         <Col>
           <h1>Next.js React Bootstrap</h1>
         </Col>
       </Row>
-    </Container>)
+      </div>
+    )
 
   }
 
