535024779a0f77790775b5115fef240eedaa91b3
michal.trajdos <michal.trajdos@fdmgroup.com>
2020-12-22 12:03:59 +0000

    Fixed project setup, home component now shows on the main page

---
+++ solo-projects/react/nextjs/dm-design/pages/_app.js
@@ -0,0 +1,11 @@
+// ensure all pages have Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './index.js';
+ 
+function MyApp() {
+ 
+  return (<Home/>)
+ 
+}
+ 
+export default MyApp;
--- solo-projects/react/nextjs/dm-design/pages/index.js
+++ solo-projects/react/nextjs/dm-design/pages/index.js
@@ -1,53 +1,21 @@
-import Head from 'next/head'
+import React, {Component} from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+ 
+export default class Home extends React.Component {
 
-export default function Home() {
-  
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  render() {
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    return (<Container>
+      <Row>
+        <Col>
+          <h1>Next.js React Bootstrap</h1>
+        </Col>
+      </Row>
+    </Container>)
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+  }
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  )
 }
