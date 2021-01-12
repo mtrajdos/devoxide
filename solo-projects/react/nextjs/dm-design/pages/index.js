277d7297a048d08a191ed76c5f903157474acc7f
michal.trajdos <michal.trajdos@fdmgroup.com>
2021-01-12 15:54:25 +0000

    Working navbar class added using bootstrap, needs link definition and better formatting for navbar space

--- solo-projects/react/nextjs/dm-design/pages/index.js
+++ solo-projects/react/nextjs/dm-design/pages/index.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
 import Row from 'react-bootstrap/Row';
 import TopBar from './topbar.js';
+import NavigationBar from './navigation-bar.js';
 import '../styling.css';
  
 export default class Home extends Component {
 
   render() {
 
-    return (<div className="container">
-      <Row>
-          <h1>Home component</h1>
-      </Row>
-      <Row>
-          <TopBar></TopBar>
-      </Row>
-      </div>
+    return (
+            <div className="container">
+              <Row>
+                <TopBar></TopBar>
+              </Row>
+
+              <Row>
+                <NavigationBar></NavigationBar>
+              </Row>
+            </div>
     )
 
   }
