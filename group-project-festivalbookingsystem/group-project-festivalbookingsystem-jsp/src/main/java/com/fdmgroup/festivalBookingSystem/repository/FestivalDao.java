3c2e89d6cea3ce6a2a04f4740c772e1ae0e321d0
Michał Trajdos <michal.trajdos95@gmail.com>
2022-05-20 13:53:52 +0200

    Delete group-project-festivalbookingsystem/group-project-festivalbookingsystem-jsp directory

--- group-project-festivalbookingsystem/group-project-festivalbookingsystem-jsp/src/main/java/com/fdmgroup/festivalBookingSystem/repository/FestivalDao.java
+++
@@ -1,9 +0,0 @@
-package com.fdmgroup.festivalBookingSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fdmgroup.festivalBookingSystem.model.Festival;
-
-public interface FestivalDao extends JpaRepository<Festival, Long> {
-	
-	}
--- group-project-festivalbookingsystem/group-project-festivalbookingsystem-jsp/src/main/java/com/fdmgroup/festivalBookingSystem/repository/TicketServiceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.fdmgroup.festivalBookingSystem.repository;
-
-public interface TicketServiceRepository<Ticket> {
-
-	void save(Ticket ticket);
-}
