45956fb4d83ffcac7311f01a7fc91d1e5be8b75b
Michal Trajdos <michal.trajdos@brightside.nl>
2022-05-24 14:54:50 +0200

    Added ERB files and solved bugs in data processing of model Python script

--- Python/zMaxResearchProject/myModelToTransferLearning.py
+++ Python/zMaxResearchProject/myModelToTransferLearning.py
@@ -14,57 +14,15 @@ import time
 # Ask for user input to pick the raw .edf file
 print("Please select the raw .edf file of the sleep recording", file=sys.stderr)
 edfPath = filedialog.askopenfilename(filetypes=[("Edf Files", "*.edf")])
-edfFile_EEG_left_uV = mne.io.read_raw_edf(edfPath, preload=True)
-
-
-# Check the data types in the recording prior to transformation
-print(edfFile_EEG_left_uV.get_data().dtypes)
-
-# Make extra copies for other channel derivations
-edfFile_EEG_right_uV = edfFile_EEG_left_uV
-edfFileEEG_mixedChannel_uV = edfFile_EEG_right_uV
-edfFileAccelerometer_X = edfFile_EEG_right_uV
-edfFileAccelerometer_Y = edfFile_EEG_right_uV
-edfFileAccelerometer_Z = edfFile_EEG_right_uV
-edfFilePPG = edfFile_EEG_right_uV
-edfFileAudio_dB = edfFile_EEG_right_uV
+edfFile_EEG_right_uV = mne.io.read_raw_edf(edfPath, preload=True)
 
 # Extract EEG right (uV) channel
-edfFile_EEG_right.drop_channels(
+edfFile_EEG_right_uV.drop_channels(
     ['EEG left (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
      'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
 
-# Extract EEG left (uV) channel
-edfFile_EEG_left.drop_channels(
-    ['EEG right (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
+# Save the data as a pandas DataFrame so it is easier to work with
+eegRightDF = pd.DataFrame(edfFile_EEG_right_uV.get_data()[0], columns = ['EEG right (uV)'])
 
-# Extract Mixed (uV) channel
-edfFileEEG_mixedChannel_uV.drop_channels(
-    ['EEG left (uV)', 'EEG right (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Status'])
-
-# Extract Accelerometer X axis channel
-edfFileEEG_accelerometer_X.drop_channels(
-    ['EEG left (uV)', 'EEG right (uV)', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
-
-# Extract Accelerometer Y axis channel
-edfFileEEG_accelerometer_Y.drop_channels(
-    ['EEG left (uV)', 'EEG right (uV)', 'Accelerometer X', 'Accelerometer Z', 'PPG', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
-
-# Extract Accelerometer Z axis channel
-edfFileEEG_accelerometer_Z.drop_channels(
-    ['EEG left (uV)', 'EEG right (uV)', 'Accelerometer X', 'Accelerometer Y', 'PPG', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
-
-# Extract PPG channel
-edfFilePPG.drop_channels(
-    ['EEG left (uV)', 'EEG right (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
-
-# Extract Audio (dB) axis channel
-edfFileAudio_dB.drop_channels(
-    ['EEG left (uV)', 'EEG right (uV)', 'Accelerometer X', 'Accelerometer Y', 'PPG', 'Accelerometer Z', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Mixed Channel', 'Status'])
+# Confirm a successful transformation to DataFrame by printing data types in new table/dataframe
+print(eegRightDF.dtypes)
