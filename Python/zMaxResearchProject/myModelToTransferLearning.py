2e099b117ea4729de86fa653d8ae7669d353877d
Michal Trajdos <michal.trajdos@brightside.nl>
2022-05-27 16:51:35 +0200

    Create an edfData variable (numpy array) and extract from there directly

--- Python/zMaxResearchProject/myModelToTransferLearning.py
+++ Python/zMaxResearchProject/myModelToTransferLearning.py
@@ -20,3 +20,39 @@ edfFile = mne.io.read_raw_edf(edfPath, preload=True)
 # ['EEG right (uV)', 'EEG left (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
 #  'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status']
 print("Channels in current edf: " + str(edfFile.ch_names))
+
+# Extract data
+edfData = edfFile.get_data()
+
+# ---- Extract separate derivations from the sleep recording ----#
+# Extract EEG right (uV) channel
+EEG_right_uV = edfData[0]
+
+# Extract the mixed EEG (uV) channel
+EEG_mixed_uV = edfData[10]
+
+# Extract EEG left (uV) channel
+EEG_left_uV = edfData[1]
+
+# Extract accelerometer data for axis X
+Accelerometer_X = edfData[2]
+
+# Extract accelerometer data for axis X
+Accelerometer_Y = edfData[3]
+
+# Extract accelerometer data for axis X
+Accelerometer_Z = edfData[4]
+
+# Extract PPG channel
+PPG = edfData[5]
+
+# Extract audio (dB) channel
+Audio_dB = edfData[9]
+
+# Extract the battery status
+Battery_V = edfData[8]
+
+# Extract the number of readings by splitting and stripping
+# the string that shows upon raw edf call, e.g.
+# <RawEDF | 21-03-2022.edf, 12 x 9050880 (35355.0 s), ~828.6 MB, data loaded>
+readings = int(str(edfFile).split(",")[1].split("x")[1].split("(")[0])  # | readings = 90550880
