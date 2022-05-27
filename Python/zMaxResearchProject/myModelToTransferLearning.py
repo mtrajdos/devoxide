6250c9ba2af8b7c88f019b1a7ce764accd0f0ecf
Michal Trajdos <michal.trajdos@brightside.nl>
2022-05-27 16:48:46 +0200

    Changed the approach for extracting derivations, display channels first

--- Python/zMaxResearchProject/myModelToTransferLearning.py
+++ Python/zMaxResearchProject/myModelToTransferLearning.py
@@ -1,5 +1,4 @@
-# Import required libraries
-import pandas as pd
+# Import libraries
 import os
 import sys
 import mne
@@ -11,18 +10,13 @@ from tkinter import ttk
 import matplotlib.pyplot as plt
 import time
 
+# ---- Load the data for training the model ----#
 # Ask for user input to pick the raw .edf file
 print("Please select the raw .edf file of the sleep recording", file=sys.stderr)
 edfPath = filedialog.askopenfilename(filetypes=[("Edf Files", "*.edf")])
-edfFile_EEG_right_uV = mne.io.read_raw_edf(edfPath, preload=True)
+edfFile = mne.io.read_raw_edf(edfPath, preload=True)
 
-# Extract EEG right (uV) channel
-edfFile_EEG_right_uV.drop_channels(
-    ['EEG left (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
-     'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status'])
-
-# Save the data as a pandas DataFrame so it is easier to work with
-eegRightDF = pd.DataFrame(edfFile_EEG_right_uV.get_data()[0], columns = ['EEG right (uV)'])
-
-# Confirm a successful transformation to DataFrame by printing data types in new table/dataframe
-print(eegRightDF.dtypes)
+# Display order of the channels for correct isolation of single derivation data, e.g. from BrainMax:
+# ['EEG right (uV)', 'EEG left (uV)', 'Accelerometer X', 'Accelerometer Y', 'Accelerometer Z', 'PPG', 'Body temperature',
+#  'Room illuminatio', 'Battery (V)', 'Audio (dB)', 'Mixed Channel', 'Status']
+print("Channels in current edf: " + str(edfFile.ch_names))
